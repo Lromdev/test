b62ba30ea0f73ebc558dd19d5a4335f9e1c8ac8b
Lromdev <153156442+Lromdev@users.noreply.github.com>
2024-06-12 12:33:20 +0300

    Update twrp_sapphiren.mk

--- twrp_sapphiren.mk
+++ twrp_sapphiren.mk
@@ -4,8 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Configure base.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from sapphiren device
